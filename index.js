4beb7f2a96186be53662a51c79c40c9455968d31
Liz H. <137332408+liz-herbert@users.noreply.github.com>
2025-04-22 19:43:51 -0400

    Update index.js
    Added comments to index.js

--- index.js
+++ index.js
@@ -7,9 +7,11 @@ const shoppingList = []
     
 addItemBtn.addEventListener('click', function(){
 
+        //if the item is already included in list, then clear the input field
         if(shoppingList.includes(itemInput.value)){
             console.log('no duplicates')
         }
+        //else add the item to the shopping list and render out of the list
         else{
             shoppingList.push(itemInput.value)
             render()        
@@ -17,6 +19,7 @@ addItemBtn.addEventListener('click', function(){
         itemInput.value = '' 
 })
 
+//add each item to the shopping list
 function render(){    
     let html = ''
     for (let item of shoppingList){
